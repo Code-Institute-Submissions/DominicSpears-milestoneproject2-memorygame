8ce32745b283077bb54dfc05969482f7eb315328
DominicSpears <dominicspears24@gmail.com>
2021-03-11 18:54:04 +0000

    add to js, instructinos modal controls

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+var modal = document.getElementById("instructionsModal");
+var btn = document.getElementById("instructions-btn");
+var span = document.getElementsByClassName("close-btn")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
