695132be104ece6ad5a9679894c7bea5a6db0bab
DominicSpears <dominicspears24@gmail.com>
2021-03-11 19:10:59 +0000

    add to js, flip card function

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,29 @@ var audio = document.getElementById("myaudio");
     audio.volume = 0.2;
 
 
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+}
+
 
 var modal = document.getElementById("instructionsModal");
 var btn = document.getElementById("instructions-btn");
