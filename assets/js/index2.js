996d3668e23da980446ad1c0f307e5b1e7599c05
DominicSpears <dominicspears24@gmail.com>
2021-03-13 19:16:44 +0000

    add to js, shuffle cards function, successful test

--- assets/js/index2.js
+++ assets/js/index2.js
@@ -41,17 +41,27 @@ class MatchThePairs {
         this.timeRemaining = this.totalTime;
         this.matchedCards = [];
         this.busy = true;
-    }
 
+        this.shuffleCards();
+    }
     flipCard(card) {
         if(this.canFlipCard(card)) {
             //count flips as clicked
             this.totalClicks++;
             this.ticker.innerText = this.totalClicks;
             card.classList.add('visible');
+
+            //if statement match check
         }
     }
-
+    //shuffle function, fisher yates function
+    shuffleCards() {
+        for(let i = this.cardsArray.length - 1; i > 0; i--) {
+            let randIndex = Math.floor(Math.random() * (i+1));
+            this.cardsArray[randIndex].style.order = i;
+            this.cardsArray[i].style.order = randIndex;
+        }
+    }
 //check "if card if ok to flip" boolean  
     canFlipCard(card) {
         return true;
