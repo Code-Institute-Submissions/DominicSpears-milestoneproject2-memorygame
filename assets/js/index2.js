8a9501bc067dcbcd3b92a472688fcc5ee1ad37c7
DominicSpears <dominicspears24@gmail.com>
2021-03-13 11:14:00 +0000

    add to js, new file for javascript test

---
+++ assets/js/index2.js
@@ -0,0 +1,11 @@
+//make sure page is loaded before javascript runs
+if(document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", ready());
+} else {
+    ready();
+}
+
+function ready() {
+    let overlays = Array.from[document.getElementsByClassName("")]
+
+}
