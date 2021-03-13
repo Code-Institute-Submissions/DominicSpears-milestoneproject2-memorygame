1d09247a76d78c5dab9d029d284e240a9765012a
DominicSpears <dominicspears24@gmail.com>
2021-03-13 13:56:51 +0000

    add to js, modal visibility code

--- assets/js/index2.js
+++ assets/js/index2.js
@@ -1,11 +1,28 @@
-//make sure page is loaded before javascript runs
-if(document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", ready());
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new MixOrMatch(100, cards);
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+            game.startGame();
+        });
+    });
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            game.flipCard(card);
+        });
+    });
+}
+
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
 } else {
     ready();
 }
 
-//make arrays from individual overlays and cards
+/*make arrays from individual overlays and cards
 function ready() {
     let overlays = Array.from[document.getElementsByClassName("overlay-text")]
     let cards = Array.from[document.getElementsByClassName("card")]
@@ -14,8 +31,20 @@ function ready() {
     overlays.forEach(overlay => {
         overlay.addEventListener("click", () => {
             overlay.classList.remove("visible");
-            //start game
-        
+            //start game        
+        });
+    });
+
+    cards.forEach(card => {
+        card.addEventListener("click", () => {
+//flipcard function
         });
     });
 }
+
+//make sure page is loaded before javascript runs
+if(document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", ready());
+} else {
+    ready();
+}*/
