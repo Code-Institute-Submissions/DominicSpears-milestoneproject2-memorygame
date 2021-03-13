7b1cbc4a9b4b68d30e5a868ba4e95d31b7b64bf0
DominicSpears <dominicspears24@gmail.com>
2021-03-13 14:36:26 +0000

    add to js, working start modal

--- assets/js/index2.js
+++ assets/js/index2.js
@@ -1,31 +1,7 @@
+//make arrays from individual overlays and cards
 function ready() {
-    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
-    let cards = Array.from(document.getElementsByClassName('card'));
-    let game = new MixOrMatch(100, cards);
-
-    overlays.forEach(overlay => {
-        overlay.addEventListener('click', () => {
-            overlay.classList.remove('visible');
-            game.startGame();
-        });
-    });
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            game.flipCard(card);
-        });
-    });
-}
-
-if(document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', ready());
-} else {
-    ready();
-}
-
-/*make arrays from individual overlays and cards
-function ready() {
-    let overlays = Array.from[document.getElementsByClassName("overlay-text")]
-    let cards = Array.from[document.getElementsByClassName("card")]
+    let overlays = Array.from(document.getElementsByClassName("overlay-text"));
+    let cards = Array.from(document.getElementsByClassName("card"));
 
 //each time you click on an overlay
     overlays.forEach(overlay => {
@@ -47,4 +23,4 @@ if(document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", ready());
 } else {
     ready();
-}*/
+}
